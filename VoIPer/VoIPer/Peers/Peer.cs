c790381076c69dc48d9838c543276b84d94db421
unknown <yrezehi@gmail.com>
2023-12-24 09:56:31 -0800

    add: peer

---
+++ VoIPer/VoIPer/Peers/Peer.cs
@@ -0,0 +1,16 @@
+﻿using SIPSorcery.Net;
+
+namespace VoIPer.Peers
+{
+    public class Peer
+    {
+        public int Id;
+        public RTCPeerConnection Connection { get; set; }
+        public DateTime JoinTime { get; set; }
+
+        public Peer() {
+            Connection = new RTCPeerConnection();
+            JoinTime = DateTime.Now;
+        }
+    }
+}
