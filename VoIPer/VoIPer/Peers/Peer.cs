07958a526ec0cc07930c915a5000fb767363ebda
unknown <yrezehi@gmail.com>
2023-12-26 08:37:11 -0800

    setup: ui

--- VoIPer/VoIPer/Peers/Peer.cs
+++ VoIPer/VoIPer/Peers/Peer.cs
@@ -1,16 +1,35 @@
 ﻿using SIPSorcery.Net;
+using VoIPer.Utils;
 
 namespace VoIPer.Peers
 {
     public class Peer
     {
-        public int Id;
+        public string Id;
         public RTCPeerConnection Connection { get; set; }
         public DateTime JoinTime { get; set; }
 
         public Peer() {
-            Connection = new RTCPeerConnection();
+            Id = IdentifierUtil.GenerateShort();
             JoinTime = DateTime.Now;
+            Connection = new RTCPeerConnection(null);
         }
+
+        public async Task OnConnect()
+        {
+            Connection.onconnectionstatechange += state =>
+            {
+                var x = state switch
+                {
+                    RTCPeerConnectionState.connected ,
+                    RTCPeerConnectionState.failed => "Case 2",
+                    RTCPeerConnectionState.closed => "Case 3",
+                    _ => throw new Exception("Unhandled case: " + state)
+                };
+            };
+        }
+
+        public static Peer Create() =>
+            new Peer();
     }
 }
---
+++ VoIPer/VoIPer/Utils/IdentifierUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoIPer.Utils
+{
+    public static class IdentifierUtil
+    {
+        public static string GenerateShort() =>
+            Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+    }
+}
